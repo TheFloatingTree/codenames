a0eb4a67529b8d24ea258bea0e0e5c73217e362e
thefloatingtree <thefloatingtree@gmail.com>
2021-06-17 18:56:38 -0600

    Added tile types and tile colors

--- client/src/components/GameTile.jsx
+++ client/src/components/GameTile.jsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { Center, ScaleFade, useColorModeValue, useMediaQuery } from "@chakra-ui/react"
 import { motion } from 'framer-motion'
+import { TILE_BLUE_TEAM, TILE_BOMB, TILE_NO_TEAM, TILE_RED_TEAM } from '../constants'
 
 const MotionCenter = motion(Center)
 
-export default function GameTile({ word, delay }) {
+const boxColors = {
+    [TILE_NO_TEAM]: ["blackAlpha.200", "whiteAlpha.200"],
+    [TILE_RED_TEAM]: ["red.300", "red.600"],
+    [TILE_BLUE_TEAM]: ["blue.300", "blue.600"],
+    [TILE_BOMB]: ["blackAlpha.600", "blackAlpha.900"],
+}
+
+export default function GameTile({ word, delay, type }) {
 
     const [shouldAppear, setShouldAppear] = useState(false)
+    const [typeHidden, setTypeHidden] = useState(true)
 
     const [desktop] = useMediaQuery("(min-width: 769px)")
     const lightMode = useColorModeValue(true, false)
 
-    const boxSize = desktop ? { w: "160px", h: "120px" } : { w: "80px", h: "60px" }
-    const boxColor = lightMode ? "blackAlpha.200" : "blackAlpha.700"
+    const boxSize = desktop ? { w: "200px", h: "160px" } : { w: "80px", h: "60px" }
+    const boxColor = typeHidden ? boxColors[TILE_NO_TEAM][+!lightMode] : boxColors[type][+!lightMode]
 
 
     useEffect(() => {
@@ -22,8 +31,24 @@ export default function GameTile({ word, delay }) {
 
     return (
         <ScaleFade in={shouldAppear} initialScale={0.5}>
-            <MotionCenter w={boxSize.w} h={boxSize.h} _hover={{ cursor: "pointer" }} backgroundColor={boxColor} border="3px" borderColor="blue.500" borderRadius="md" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
-                <div style={{ userSelect: "none" }}>{word}</div>
+            <MotionCenter
+                onClick={() => setTypeHidden(false)}
+                w={boxSize.w}
+                h={boxSize.h}
+                _hover={{ cursor: "pointer" }}
+                backgroundColor={boxColor}
+                border="3px"
+                borderColor="blue.500"
+                borderRadius="md"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.9 }}>
+                <div style={{
+                    userSelect: "none",
+                    textTransform: "uppercase",
+                    hyphens: "auto",
+                    margin: "1em",
+                    fontWeight: "bold"
+                }}>{word}</div>
             </MotionCenter>
         </ScaleFade>
     )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,16 +2,29 @@ import React, { useEffect, useState } from 'react'
 import { Button, Center, Flex, Grid, Spacer, useColorMode } from "@chakra-ui/react"
 import { SettingsIcon } from '@chakra-ui/icons'
 import GameTile from '../components/GameTile'
+import { TILE_BLUE_TEAM, TILE_BOMB, TILE_NO_TEAM, TILE_RED_TEAM } from '../constants'
+
+const randomChoice = arr => {
+    const randIndex = Math.floor(Math.random() * arr.length);
+    return arr[randIndex];
+};
 
 export default function Home() {
 
     const { colorMode, toggleColorMode } = useColorMode()
-    const [words, setWords] = useState([])
+    const [tiles, setTiles] = useState([])
 
     useEffect(() => {
         fetch('/api/get/words')
             .then(res => res.json())
-            .then(setWords)
+            .then(words => {
+                setTiles(words.map(word => {
+                    return {
+                        word,
+                        type: randomChoice([ TILE_NO_TEAM, TILE_RED_TEAM, TILE_BLUE_TEAM, TILE_BOMB ])
+                    }
+                }))
+            })
     }, [])
 
     return (
@@ -24,8 +37,8 @@ export default function Home() {
             </Flex>
             <Center>
                 <Grid templateColumns="repeat(5, 1fr)" gap="3">
-                    {words.map((word, index) => {
-                        return <GameTile delay={index * 3} word={word} key={word}></GameTile>
+                    {tiles.map((tile, index) => {
+                        return <GameTile delay={index * 3} type={tile.type} word={tile.word} key={tile.word}></GameTile>
                     })}
                 </Grid>
             </Center>
